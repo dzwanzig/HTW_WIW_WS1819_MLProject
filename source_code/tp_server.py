72c5d665d929550c78ecca6e9389b254e0fda1c2
Dorian <44399149+dorianzwanzig@users.noreply.github.com>
2019-01-04 16:58:09 +0100

    Update tp_server.py

--- source_code/tp_server.py
+++ source_code/tp_server.py
@@ -8,7 +8,7 @@ def get_df():
     password = "Masterprojekt"
     conn = pymssql.connect("pcs.f4.htw-berlin.de",
                            "Masterprojekt", password, "PraediktiveAnalysenTest")
-    df = pd.read_sql('SELECT * FROM Maschinendaten_20181122', conn)
+    df = pd.read_sql('SELECT * FROM Maschinendaten_20181206', conn)
     conn.close()
     return(df)
 
@@ -17,31 +17,17 @@ def get_latest(menge):
     """function returns latest n row from machine data table on project db as a list"""
     menge = str(menge)
     sql = str('SELECT TOP ' + menge +
-              ' * FROM Maschinendaten_20181122 ORDER BY ID DESC')
+              ' * FROM Maschinendaten_20181206 ORDER BY ID DESC')
     password = "Masterprojekt"
     conn = pymssql.connect("pcs.f4.htw-berlin.de",
                            "Masterprojekt", password, "PraediktiveAnalysenTest")
     df = pd.read_sql(sql, conn)
+    counter = []
+    for x in range(20, 0, -1):
+        counter.append(x)
+    df["Counter"] = counter
     conn.close()
     return(df)
-
-
-def write_sim_data(data_id_nr, machine_id, time, datum, uhrzeit, std_dz, std_la, std_vb, std_ls, std_te, fehler_id, prod_programm, soll_menge, ist_menge, ausschuss, machine_nr=5):
-    """ function takes following variables:
-    data_id_nr, machine_id, time, datum, uhrzeit, std_dz, std_la, std_vb, std_ls, std_te, fehler_id, prod_programm, soll_menge, ist_menge, ausschuss, machine_nr = 5
-    and writes them as a new row in to the machine data table on project db"""
-    password = "Masterprojekt"
-    conn = pymssql.connect("pcs.f4.htw-berlin.de",
-                           "Masterprojekt", password, "PraediktiveAnalysenTest")
-    cursor = conn.cursor()
-    new_row = tuple(("400_1_" + str(data_id_nr), machine_id, time, datum, uhrzeit, std_dz, std_la, std_vb, std_ls, std_te, fehler_id,
-                     prod_programm, soll_menge, ist_menge, ausschuss, machine_nr))
-    cursor.executemany(
-        "INSERT INTO Maschinendaten_20181122 VALUES (%d, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-        [new_row])
-    conn.commit()
-    conn.close()
-
 
 def read_db_20():
     """ This function connects to machine data table in the prediction database, reads the last 20 rows and stores them with a counter in a pandas data frame.
@@ -50,10 +36,11 @@ def read_db_20():
     conn = pymssql.connect("pcs.f4.htw-berlin.de",
                            "Masterprojekt", password, "PraediktiveAnalysenTest")
     df = pd.read_sql(
-        'SELECT TOP 20 * FROM Maschinendaten_20181122 ORDER BY ID DESC', conn)
+        'SELECT TOP 20 * FROM Maschinendaten_20181206 ORDER BY ID DESC', conn)
     # adding the counter
-    counter = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8,
-               7, 6, 5, 4, 3, 2, 1]
+    counter = []
+    for x in range(20, 0, -1):
+        counter.append(x)
     df["Counter"] = counter
     return(df)
 
