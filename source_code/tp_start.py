b61e3f83bbc01adacee4ea65d60d38032bbce8f4
Dorian <44399149+dorianzwanzig@users.noreply.github.com>
2019-01-04 16:57:31 +0100

    Update tp_start.py

--- source_code/tp_start.py
+++ source_code/tp_start.py
@@ -1,6 +1,6 @@
-from ml_tools import lin_reg, log_reg_predict, knn_predict, poly_log_reg_predict
-from server import get_latest, read_db_20, rul_write
-from helper import wait
+from tp_ml_tools import lin_reg, log_reg_predict, knn_predict, poly_log_reg_predict
+from tp_server import get_latest, read_db_20, rul_write
+from tp_helper import wait
 
 while True:
     df_latest = get_latest(1)
@@ -9,6 +9,7 @@ while True:
     prediction_log_reg = log_reg_predict(df_latest)
     prediction_knn = knn_predict(df_latest)
     prediction_poly = poly_log_reg_predict(df_latest)
+    rul = (int(prediction_knn[0]/2) + int(prediction_lin_reg/60)) / 2
     print("---------------------------------")
     print("Latest power consumption value: " +
           str(df_latest["Leistungsaufnahme"].values) + " // Latest temperatur value: " + str(df_latest["Temperatur"].values))
@@ -16,15 +17,15 @@ while True:
     print("Log Reg Prediction: Failure in next 25 minutes ?" +
           str(prediction_log_reg))
     if prediction_log_reg == [True]:
-        print("KNN Prediction : Failure in " +
-              str(prediction_knn[0]/2) + " minutes")
-        print("Lin Reg Prediction: Failure in " +
-              str(prediction_lin_reg/60) + " minutes")
-        print("Polynominal Lin Reg Prediction: Next Failure is F001? " +
-              str(prediction_poly))
-        if prediction_poly == [0]:
-            fail = "F002"
-        else:
-            fail = "F001"
-        rul_write(df_lin_reg, prediction_lin_reg, failure=fail)
+      print("KNN Prediction : Failure in " +
+            str(prediction_knn[0]/2) + " minutes")
+      print("Lin Reg Prediction: Failure in " +
+            str(prediction_lin_reg/60) + " minutes")
+      print("Polynominal Lin Reg Prediction: Next Failure is F001? " +
+            str(prediction_poly))
+      if prediction_poly == [0]:
+          fail = "F002"
+      else:
+          fail = "F001"  
+      rul_write(df_lin_reg, rul, failure=fail)
     wait(5)
